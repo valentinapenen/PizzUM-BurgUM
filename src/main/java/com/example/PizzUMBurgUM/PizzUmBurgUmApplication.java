faf8fbf8711f515a796c31e3367d116800d99211
valentinapenen <valentinapenenbinttencourt@gmail.com>
2025-10-13 08:52:08 -0300

    Initial commit

---
+++ src/main/java/com/example/PizzUMBurgUM/PizzUmBurgUmApplication.java
@@ -0,0 +1,13 @@
+package com.example.PizzUMBurgUM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzUmBurgUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzUmBurgUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PizzUMBurgUM/PizzUmBurgUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PizzUMBurgUM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzUmBurgUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
