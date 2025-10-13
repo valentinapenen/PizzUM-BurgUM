2b48452fa385df823ebd1ed66d48cfcf10ad49c3
valentinapenen <valentinapenenbinttencourt@gmail.com>
2025-10-13 09:40:57 -0300

    Usuario y atributos

--- src/main/java/com/example/PizzUMBurgUM/entities/Usuario.java
+++ src/main/java/com/example/PizzUMBurgUM/entities/Usuario.java
@@ -1,4 +1,33 @@
 package com.example.PizzUMBurgUM.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Usuario {
+    @NotNull
+    private String nombre;
+    @NotNull
+    private String apellido;
+    @Id
+    @Column(length = 8)
+    @NotNull
+    private int cedula;
+    @NotNull
+    private Date fechaNacimiento;
+    @NotNull
+    private String correo;
+    @NotNull
+    private String telefono;
 }
