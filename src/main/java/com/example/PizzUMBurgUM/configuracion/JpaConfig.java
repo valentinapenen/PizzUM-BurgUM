eb3651f9c53791bffa1b2a36d9cbba6854caa47a
Ameliemu <amures@correo.um.edu.uy>
2025-11-10 14:01:42 -0300

    comentario

---
+++ src/main/java/com/example/PizzUMBurgUM/configuracion/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.example.PizzUMBurgUM.configuracion;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+public class JpaConfig {
+
+}
